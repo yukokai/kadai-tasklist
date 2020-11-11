961253576cd731ac1817b8e51ea92b363c0ef6c1
yuko.kai <ykaihome83gmail.com>
2020-11-11 10:48:58 +0000

    CRUD tasks

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -100,7 +100,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+        // タスクを更新
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
     }
 
     /**
@@ -111,6 +118,12 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+        // タスクを削除
+        $task->delete();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
     }
 }
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -15,5 +15,14 @@
             <td>{{ $task->content }}</td>
         </tr>
     </table>
+    
+    {{-- タスク編集ページへのリンク --}}
+    {!! link_to_route('task.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!} 
+    {{-- タスク削除フォーム --}}
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+    
+    
 
 @endsection
